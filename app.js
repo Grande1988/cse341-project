764ef20775589b6faa23112e4f5ba3dc6d99145f
Grande1988 <71180666+Grande1988@users.noreply.github.com>
2021-10-05 22:53:27 -0600

    MONGODB ADDED

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-const path = require('path');
-const express = require('express');
-const errorController = require('./controllers/error');
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+...
+const corsOptions = {
+    origin: "https://<your_app_name>.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 5000;
-const app = express();
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-app.use(errorController.get404);
-
-app.listen(PORT);
-
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+                        
