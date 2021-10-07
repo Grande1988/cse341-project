4199f1f1613242a3a099455f914ffad0149139fa
Grande1988 <71180666+Grande1988@users.noreply.github.com>
2021-10-07 17:45:46 -0600

    package and routes js

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write(
+            '<body><form action'
+        )
+    }
+}
